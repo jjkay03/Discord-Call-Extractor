617fa9f65609239432dd505783db9499eecef2e4
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2025-03-10 14:34:26 +0100

    Web module to login into Discord

---
+++ main.py
@@ -0,0 +1,12 @@
+from modules import * # Import all custom modules in /modules dir
+
+group_link = "https://discord.com/channels/@me/974077775638069338"
+
+driver = setup_discord_browser()
+navigate_to_link(driver, group_link)
+
+# Let the user choose when to start
+input("Press Enter to start tracking Discord calls...")
+
+# End
+input("END -- END -- END -- END")
---
+++ modules/web.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import time
+import os
+
+# Function to setup discord browser
+def setup_discord_browser():
+    options = Options()
+    
+    # Create a user profile
+    user_data_dir = os.path.join(os.getcwd(), "data/selenium_browser_profile")
+    os.makedirs(user_data_dir, exist_ok=True)
+    options.add_argument(f"--user-data-dir={user_data_dir}")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://discord.com/login")
+    
+    # Let the user login manually
+    print("Please log in to Discord. The program will continue once you're logged in.")
+    
+    # Wait for Discord to fully load by looking for the friends button container
+    WebDriverWait(driver, 300).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "[class*='friendsButtonContainer']"))
+    )
+    
+    return driver
+
+# Function that navigates browser to link
+def navigate_to_link(driver, link):
+    driver.get(link)
+    print(f"Navigated to link: {link}")
+    time.sleep(3)  
